5d482ebea6072b6362fb1d26cec772ca110e817e
sanggon6107 <sanggon6107@naver.com>
2023-02-18 14:39:18 +0900

    Feat : Implement PutCursor 타이틀 화면 커서 구현

--- Context/ContextTitleScreen.cpp
+++ Context/ContextTitleScreen.cpp
@@ -7,16 +7,17 @@ ContextState ContextTitleScreen::Run()
 	HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 	SetConsoleActiveScreenBuffer(console);
 	DWORD bytes_written = 0;
-	wstring a = L"Hello!";
 
 	while (1)
 	{
-		for (int i = 0; i < screen_height_; i++) { for (int j = 0; j < screen_width_; j++) { screen[i * screen_width_ + j] = ' '; } }
+		for (int i = 0; i < screen_height_; i++) { for (int j = 0; j < screen_width_; j++) { screen[i * screen_width_ + j] = L' '; } }
 
-		PutText(screen, TITLE_W, TITLE_H, TitleScreenText::kTitle);
-		PutText(screen, START_W, START_H, TitleScreenText::kStart);
-		PutText(screen, EXIT_W, EXIT_H, TitleScreenText::kExit);
+		PutText(screen, text_position_[(int)TitleScreenText::kTitle].w_, text_position_[(int)TitleScreenText::kTitle].h_, TitleScreenText::kTitle);
+		PutText(screen, text_position_[(int)TitleScreenText::kStart].w_, text_position_[(int)TitleScreenText::kStart].h_, TitleScreenText::kStart);
+		PutText(screen, text_position_[(int)TitleScreenText::kExit].w_, text_position_[(int)TitleScreenText::kExit].h_, TitleScreenText::kExit);
 
+		// 여기에서 키 조작으로 커서 움직이기.
+		PutCorsur(screen, TitleScreenText::kStart);
 
 		screen[screen_width_ * screen_height_ - 1] = '\0';
 		WriteConsoleOutputCharacter(console, screen, screen_width_ * screen_height_, { 0, 0 }, &bytes_written);
@@ -30,9 +31,25 @@ void ContextTitleScreen::PutText(wchar_t* screen, int w, int h, TitleScreenText 
 	//w and h should be bigger than 0.
 	for (int height = h; height < h + text_list_[(int)text_type].size(); height++)
 	{
-		wcscpy_s(&screen[screen_width_ * height + w], wcslen(screen), text_list_[(int)text_type][height - h].data());
+		wchar_t* char_to_put = text_list_[(int)text_type][height - h].data();
+		wcscpy_s(&screen[screen_width_ * height + w], wcslen(char_to_put) + 1, char_to_put);
 	}
-}																											   
+}
+
+void ContextTitleScreen::PutCorsur(wchar_t* screen, TitleScreenText text_type)
+{
+	const int top = text_position_[(int)text_type].h_;
+	int left = text_position_[(int)text_type].w_ - wcslen(cursor_.image_[0].data()) - cursor_.horizontal_padding_;
+
+	if (left < 1) left = 1;
+
+	for (int height = top; height < top + cursor_.image_.size(); height++)
+	{
+		wchar_t* char_to_put = cursor_.image_[height - top].data();
+		wcscpy_s(&screen[screen_width_ * height + left], wcslen(char_to_put) + 1, char_to_put);
+	}
+}
+
 																											   
 ContextTitleScreen::ContextTitleScreen(int w, int h) : IContext(w, h)										   
 {																											   
@@ -67,5 +84,17 @@ ContextTitleScreen::ContextTitleScreen(int w, int h) : IContext(w, h)
 	text_list_[(int)TitleScreenText::kExit].push_back(L"███───█────█───█─");
 	text_list_[(int)TitleScreenText::kExit].push_back(L"█────███───█───█─");
 	text_list_[(int)TitleScreenText::kExit].push_back(L"███─██─██─███──█─");
+
+
+	cursor_.image_.push_back(L"──────█───");
+	cursor_.image_.push_back(L"───────██─");
+	cursor_.image_.push_back(L"██████████");
+	cursor_.image_.push_back(L"───────██─");
+	cursor_.image_.push_back(L"──────█───");
+
+	text_position_.resize((int)TitleScreenText::kTitleScreenTextSize, {.w_ = 0, .h_ = 0});
+	text_position_[(int)TitleScreenText::kTitle] = { .w_ = 90,  .h_ = 10 };
+	text_position_[(int)TitleScreenText::kStart] = { .w_ = 100, .h_ = 40 };
+	text_position_[(int)TitleScreenText::kExit]  = { .w_ = 100, .h_ = 50 };
 
 }
--- Context/ContextTitleScreen.h
+++ Context/ContextTitleScreen.h
@@ -2,13 +2,25 @@
 
 #include "IContext.h"
 
-#define TITLE_W 90
-#define TITLE_H 10
-#define START_W 100
-#define START_H 40
-#define EXIT_W  100
-#define EXIT_H  50
+//#define TITLE_W 90
+//#define TITLE_H 10
+//#define START_W 100
+//#define START_H 40
+//#define EXIT_W  100
+//#define EXIT_H  50
+//#define CUSOR_W 70
 
+struct ObjectPosition
+{
+	int w_;
+	int h_;
+};
+
+struct Cursor
+{
+	vector<wstring> image_;
+	int horizontal_padding_{5};
+};
 
 
 enum class TitleScreenText : int { kTitle, kStart, kExit, kTitleScreenTextSize };
@@ -18,7 +30,11 @@ class ContextTitleScreen : public IContext
 public :
 	ContextTitleScreen(int w, int h);
 	ContextState Run() override;
-	void PutText(wchar_t* screen, int w, int h, TitleScreenText text_type);
 private:
+	void PutText(wchar_t* screen, int w, int h, TitleScreenText text_type);
+	void PutCorsur(wchar_t* screen, TitleScreenText text_type);
+
 	vector<vector<wstring>> text_list_;
+	Cursor cursor_;
+	vector<ObjectPosition> text_position_;
 };
