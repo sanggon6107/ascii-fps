758e909e1bd3715f3867ae126e2d02947b8600db
sanggon6107 <sanggon6107@naver.com>
2023-02-23 22:06:23 +0900

    Refactor : Remove local variables in ShowCursor ShowCursor 함수의 지역 변수중 필요 없는 것 제거

--- CommonLib/Cursor.cpp
+++ CommonLib/Cursor.cpp
@@ -4,21 +4,18 @@ void Cursor::SetCursor(int title_screen_text_w, int title_screen_text_h)
 {
 	cursor_w_ = title_screen_text_w - static_cast<int>(wcslen(image_[0].data())) - horizontal_padding_;
 	cursor_h_ = title_screen_text_h;
+	if (cursor_w_ < 1) cursor_w_ = 1;
+	if (cursor_h_ < 1) cursor_h_ = 1;
 }
 
 void Cursor::ShowCursor(wchar_t* screen)
-{
-	const int top = cursor_h_;
-	int left = cursor_w_;
-
-	if (left < 1) left = 1;
-	
-	for (int height = top; height < top + static_cast<int>(image_.size()); height++)
+{	
+	for (int height = cursor_h_; height < cursor_h_ + static_cast<int>(image_.size()); height++)
 	{
-		wchar_t* char_to_put = image_[height - top].data();
+		wchar_t* char_to_put = image_[height - cursor_h_].data();
 		for (int x_pos = 0; x_pos < wcslen(char_to_put); x_pos++)
 		{
-			screen[screen_width_ * height + left + x_pos] = image_[height - top][x_pos];
+			screen[screen_width_ * height + cursor_w_ + x_pos] = image_[height - cursor_h_][x_pos];
 		}
 	}
 }
